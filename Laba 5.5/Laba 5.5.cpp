26aa93fdaa298318d71c44e9ce9242fe130b021a
krabster <bohdan.kzymyr.ik.2022@lpnu.ua>
2022-11-08 14:47:50 +0200

    f

---
+++ Laba 5.5/Laba 5.5.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double f(int  x, int  n);
+double c(int n, int a);
+ 
+int main()
+{
+    double t, n, x;
+    int level = 0;
+    cout << "n = "; cin >> n;
+    cout << "x = "; cin >> x;
+    t = f(x, n);
+    cout << "level of recource = " << c(n, level) << endl;
+    cout << "depth of a recursive = " << f(n, x) << endl;
+    cout << "x = " << t << endl;
+    return 0;
+
+}
+double f(int  x, int  n)
+{
+
+    if (n == 0)
+        return 1;
+    if (n < 0)
+        return (1.0/(pow(x, abs(n))));
+    else
+
+        return (x * pow(x, (n - 1)));
+
+}
+double c(int n, int a)
+{
+    if (n != 0)
+    {
+        if (n % 2 == 0)
+        {
+            return c(n / 2, a);
+        }
+        else
+        {
+            return c(n / 2, ++a);
+        }
+    }
+    else
+        return a;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Laba 5.5/Laba 5.5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = f(1, 3);
+		}
+	};
+}
